3efd832d0c705d4530604c1a35c2e20e2b236bc8
RobertEng <googlypantsroberteng@gmail.com>
2016-12-14 13:40:57 -0800

    hi

---
+++ template_config.py
@@ -0,0 +1,14 @@
+# This is just the template config file so people don't randomly push the
+# password
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://Resume:@localhost/people'
+
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'ubuntu',
+    'password': '',
+    'database': 'hacktech'
+}

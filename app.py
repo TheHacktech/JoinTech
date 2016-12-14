120fba639764e2c2864eae2ee11db06b3f0cd23a
RobertEng <googlypantsroberteng@gmail.com>
2016-12-14 15:46:15 -0800

    remove localhost

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:////tmp/test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hacktech'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///hacktech'
 db = SQLAlchemy(app)
 
 

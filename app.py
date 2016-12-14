cc25a89fa8ac2ad8eb8938d4b132c5cb6a00c569
RobertEng <googlypantsroberteng@gmail.com>
2016-12-14 15:36:07 -0800

    trying simple server

--- app.py
+++ app.py
@@ -1,45 +1,23 @@
-from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:////tmp/test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hacktech'
 db = SQLAlchemy(app)
 
-# Create our database model
+
 class User(db.Model):
-    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
 
-    def __init__(self, email):
+    def __init__(self, username, email):
+        self.username = username
         self.email = email
 
     def __repr__(self):
-        return '<E-mail %r>' % self.email
-
-# Set "homepage" to index.html
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Save e-mail to database and send to success page
-@app.route('/prereg', methods=['POST'])
-def prereg():
-    email = None
-    if request.method == 'POST':
-        email = request.form['email']
-        # Check that email does not already exist (not a great query, but works)
-        if not db.session.query(User).filter(User.email == email).count():
-            reg = User(email)
-            db.session.add(reg)
-            db.session.commit()
-            return render_template('success.html')
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
-
+        return '<User %r>' % self.username
 
 # # from flask import Flask, render_template
 # # from flask_sqlalchemy import SQLAlchemy

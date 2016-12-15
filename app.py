7791d108b0f4e47ec5571db20e7a7825a9141209
RobertEng <googlypantsroberteng@gmail.com>
2016-12-15 00:41:34 -0800

    copypasta the current sites frontend to this repo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static/assets')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:////tmp/test.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///hacktech'
 db = SQLAlchemy(app)
@@ -22,7 +22,7 @@ class User(db.Model):
 
 @app.route('/')
 def index():
-    return 'Flask is running!'
+    return render_template('index.html')
 
 
 @app.route('/data')
@@ -34,51 +34,3 @@ def names():
 if __name__ == '__main__':
     app.run()
 
-# # from flask import Flask, render_template
-# # from flask_sqlalchemy import SQLAlchemy
-
-# # app = Flask(__name__)
-# # app.config.from_pyfile('config.py')
-# # db = SQLAlchemy(app)
-
-# # class Person(db.Model):
-# #     id = db.Column(db.Integer, primary_key=True)
-# #     name = db.Column(db.String(80), unique=True)
-# #     age = db.Column(db.Integer)
-# #     school = db.Column(db.String(200))
-
-# #     def __init__(self, name, age, school):
-# #         self.name = name
-# #         self.age = age
-# #         self.school = school
-
-# #     def __repr__(self):
-# #         return '<%r>' % self.name
-
-# # @app.route('/')
-# # def show_people():
-# #     info = []
-# #     for person in Person.query.all():
-# #         info.append((person.name, person.age, person.school))
-# #     return render_template('index.html', info=info)
-
-
-
-# from flask import Flask, jsonify
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def index():
-#     return 'Flask is running!'
-
-
-# @app.route('/data')
-# def names():
-#     data = {"names": ["Jhen", "Jandrew", "Jrace", "Jenny", "Jeng", "Jadvith"]}
-#     return jsonify(data)
-
-
-# if __name__ == '__main__':
-#     app.run()
